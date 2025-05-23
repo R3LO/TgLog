e76dae72f59905b4df6b7d8413d9b5ee4d3299ab
Vladimir Pavlenko <r3lo@ruqrz.com>
2025-05-23 21:00:23 +0300

    Загрузка лога

--- handlers/CallBacksMenu.py
+++ handlers/CallBacksMenu.py
@@ -5,6 +5,7 @@ from aiogram.types import CallbackQuery
 from aiogram.fsm.context import FSMContext
 from state.uload_log import Upload_logState
 from keyboards.inline_menu_kb import interlinemenu
+from utils.database import Database
 import os
 
 
@@ -14,7 +15,18 @@ import os
 async def CallBaksMenu(callback: CallbackQuery, state: FSMContext, bot: Bot):
     if (callback.data == 'upload_log'):
         await callback.message.delete()
-        await callback.message.answer('<b>Выбрано</b>: Загрузить лог')
+        await callback.message.answer(f'<b>Выбрано</b>: Загрузить лог')
+        await callback.message.answer(f'💡 <i><b>ADIF файл должен содержать следующие теги:</b></i> \n'
+                                        f'▫️QSO_DATE \n'
+                                        f'▫️TIME_ON \n'
+                                        f'▫️CALL \n'
+                                        f'▫️MODE \n'
+                                        f'▫️SUBMODE (необязательное поле для цифровых видов) \n'
+                                        f'▫️BAND \n'
+                                        f'▫️GRIDSQUARE \n'
+                                        f'▫️MY_GRIDSQUARE \n\n'
+                                        f'💾 <b>Для загрузки нажмите ниже 📎 и выбирте ваш файл ADIF лога для загрузки в систему.</b> \n\n'
+                                        f'<i>Для отмены загружки отправьте текстовое сообшние <b>отмена</b></i>')
         await state.set_state(Upload_logState.upload_adif)
 
 
@@ -24,21 +36,43 @@ async def CallBaksMenu(callback: CallbackQuery, state: FSMContext, bot: Bot):
 
 
 async def upload_adif(message: types.Message, state: FSMContext, bot: Bot):
-
+    db = Database(os.getenv('DATABASE_NAME'))
+    users = db.select_user_id(message.from_user.id)
     if message.document:
-        download_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs.txt')
+        file = 'logs\\' + users[1] + '_' + str(message.from_user.id) +'.txt'
+        download_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
         await message.bot.download(message.document, destination=download_path)
-        await state.clear()
-        f = open(download_path, 'r')
-        print('начинаю проверку...')
-        log = f.read()
-        # print(log)
-        await bot.send_message(message.from_user.id, '<b>Лог получен</b> 👍 \n\n', reply_markup=interlinemenu())
-        # print(file_path)
-        # all_media_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'All_logs')
-        # adif_file = FSInputFile(path=os.path.join(all_media_dir, 'temp.txt'))
-        # file = await message.bot.get_file(audio_file(adif_file)
+        await bot.send_message(message.from_user.id, '👍 Дождитесь результата обработки...\n\n')
+        await bot.send_message(message.from_user.id, '✅ Лог получен. Обработка QRX...\n\n')
+        await adif(users[1], download_path, message, bot, state)
 
-        # await message.answer("Ваше аудиосообщение принято")
+        # await state.clear()
+        # await bot.send_message(message.from_user.id, 'Для продолжения выбирте действие', reply_markup=interlinemenu())
+
+
     else:
-        await message.reply("⛔️ Отправьте файл лог в формате ADIF")
+        await message.reply("⛔️ Загрузка лога отменена")
+        await state.clear()
+        await bot.send_message(message.from_user.id, 'Для продолжения выбирте действие', reply_markup=interlinemenu())
+
+
+
+
+async def adif(table_db: str, log: str, message: Message, bot: Bot, state):
+    f = open(log, 'r')
+    try:
+        log = f.read().upper()
+    except:
+        pass
+    # db = Database(os.getenv('DATABASE_NAME'))
+
+    if ('<EOH>' in log) and ('<QSO_DATE:' in log) and ('<TIME_ON:' in log) and ('<BAND:' in log) and ('CALL:' in log) and ('<EOR>' in log):
+        await bot.send_message(message.from_user.id, '✅ ADIF теги в логе есть <i>(предварительная оценка)</i>...')
+        # log = log.replace("<", " <")
+        log = log.splitlines()
+        print(len(log))
+
+        await state.clear()
+    else:
+        await bot.send_message(message.from_user.id, '❌ Не найден како-то нужный тег.')
+        await bot.send_message(message.from_user.id, '🤷 Попробуйте еще раз загрузить 📎 исправленный log или введите слово <b>отмена</b> для выходя из режима загрузки лога 👇')
